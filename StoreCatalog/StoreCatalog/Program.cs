4a2290c093d5bd856c32f851a8d4d02e827e4658
Paulo Gentile <paulo_gentile@msn.com>
2019-02-11 19:46:47 -0200

    Ajuste no nome do projeto.

--- StoreCatalog/StoreCatalog/Program.cs
+++ StoreCatalog/StoreCatalog/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace StoreCatalog
+namespace GeekBurguer.StoreCatalog
 {
     public class Program
     {
