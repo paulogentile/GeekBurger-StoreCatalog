54238e7e7bf1e10e4f0bfc615518d0df14113c49
Paulo Gentile <paulo_gentile@msn.com>
2019-02-11 21:19:17 -0200

    Adicionado classes e contratos iniciais.

---
+++ StoreCatalog/StoreCatalog.Contract/ItemToGet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeekBurguer.StoreCatalog.Contract
+{
+    public class ItemToGet
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ StoreCatalog/StoreCatalog/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekBurguer.StoreCatalog.Model
+{
+    public class Product
+    {
+        [ForeignKey("StoreId")]
+        public Store Store { get; set; }
+        public Guid StoreId { get; set; }
+
+        [Key]
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+
+        public ICollection<Item> Items { get; set; } = new List<Item>();
+    }
+}
