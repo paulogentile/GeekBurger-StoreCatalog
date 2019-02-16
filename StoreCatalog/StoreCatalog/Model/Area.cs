dd44d4cdd2f26aa081afceb75e9a3396a6ce773b
Paulo Gentile <paulo_gentile@msn.com>
2019-02-16 10:44:04 -0200

    Adicionado area

---
+++ StoreCatalog/StoreCatalog/Model/Area.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeekBurger.StoreCatalog.Model
+{
+    public class Area
+    {
+        [Key]
+        public Guid AreaId { get; set; }
+
+        public Guid ProductionId { get; set; }
+
+        public string[] Restrictions { get; set; }
+
+        public bool On { get; set; }
+    }
+}
