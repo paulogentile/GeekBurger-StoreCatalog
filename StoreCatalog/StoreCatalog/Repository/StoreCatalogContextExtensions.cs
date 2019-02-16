26ab3c4871ca31ffe44a7099a1d8a4c4f6e25c67
Guilherme <uk.gentile@gmail.com>
2019-02-16 10:40:12 -0200

    Iniciando o DBContext

---
+++ StoreCatalog/StoreCatalog/Repository/StoreCatalogContextExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeekBurger.StoreCatalog.Repository
+{
+    public class StoreCatalogContextExtensions
+    {
+    }
+}
