bc3c058da5c6c66b960455010345b79536e1f462
Paulo Gentile <paulo_gentile@msn.com>
2019-02-18 21:51:04 -0300

    Ajustes nos controllers

--- StoreCatalog/StoreCatalog/Controllers/ProductsController.cs
+++ StoreCatalog/StoreCatalog/Controllers/ProductsController.cs
@@ -9,14 +9,6 @@ using System.Linq;
 //teste
 namespace GeekBurger.StoreCatalog.Controllers
 {
-    /// <summary>
-    /// Retorna todos produtos disponíveis para o cliente 
-    /// aplicando suas restrições
-    /// </summary>
-    /// <param name="query">Classe que pede o Nome da Loja, Id do Usuário e Restrições</param>
-    /// <returns>Lista de Produtos Disponíveis para Produção</returns>
-    [Produces("application/json")]
-    [Route("api/products")]
     public class ProductsController : Controller
     {
         private IHealthCheck _healthCheck;
@@ -30,30 +22,48 @@ namespace GeekBurger.StoreCatalog.Controllers
             _storeCatalogRepository = storeCatalogRepository;
         }
 
+        /// <summary>
+        /// Retorna todos produtos disponíveis para o cliente 
+        /// aplicando suas restrições
+        /// </summary>
+        /// <param name="query">Classe que pede o Nome da Loja, Id do Usuário e Restrições</param>
+        /// <returns>Lista de Produtos Disponíveis para Produção</returns>
+        [Produces("application/json")]
+        [Route("api/products")]
         [HttpGet]
         public IActionResult GetProducts(QueryProductByStore query)
         {
-            var areas = _storeCatalogRepository.GetAreas();
-            var filteredProducts = _storeCatalogRepository.GetProductsByRestrictions(query.Restrictions);
-
-            //any area allowed for this user
-            var allowedAreas = areas.Where(area => 
-                query.Restrictions.All(restriction => area.Restrictions.Any(c => c.Name.Contains(restriction))));
-
-            //products that can be produced in allowed areas for this user
-            var allowedProducts = filteredProducts.Where(product => product
-                //all ingredients from this product are not in the restriction list of at least one allowed area
-                .Ingredients.All(ingredient => allowedAreas.Any(area => !area.Restrictions.Any(c => c.Name.Contains(ingredient.Name)))));
-
-            return Ok(allowedProducts.Select(p => new ProductByStoreToGet
+            if (_healthCheck.Healthy)
             {
-                ProductId = p.ProductId,
-                Image = p.Image,
-                Items = p.Ingredients.Select(c => new ItemToGet { ItemId = c.ItemId, Name = c.Name }).ToList(),
-                Name = p.Name,
-                Price = p.Price,
-                StoreId = p.StoreId
-            }));
+                var areas = _storeCatalogRepository.GetAreas();
+                var filteredProducts = _storeCatalogRepository.GetProductsByRestrictions(query.Restrictions);
+
+                //any area allowed for this user
+                var allowedAreas = areas.Where(area =>
+                    query.Restrictions.All(restriction => area.Restrictions.Any(c => c.Name.Contains(restriction))));
+
+                //products that can be produced in allowed areas for this user
+                var allowedProducts = filteredProducts.Where(product => product
+                    //all ingredients from this product are not in the restriction list of at least one allowed area
+                    .Ingredients.All(ingredient => allowedAreas.Any(area => !area.Restrictions.Any(c => c.Name.Contains(ingredient.Name)))));
+
+                // Envia a mensagem UserWithLessOffer
+                if (allowedProducts.Count() < 4)
+                {
+                }
+
+                return Ok(allowedProducts.Select(p => new ProductByStoreToGet
+                {
+                    ProductId = p.ProductId,
+                    Image = p.Image,
+                    Items = p.Ingredients.Select(c => new ItemToGet { ItemId = c.ItemId, Name = c.Name }).ToList(),
+                    Name = p.Name,
+                    Price = p.Price,
+                    StoreId = p.StoreId
+                }));
+            }
+            else
+                return StatusCode(503);
         }
     }
 }
--- StoreCatalog/StoreCatalog/Repository/StoreCatalogRepository.cs
+++ StoreCatalog/StoreCatalog/Repository/StoreCatalogRepository.cs
@@ -1,35 +1,67 @@
-﻿using GeekBurger.StoreCatalog.Model;
+﻿using GeekBurger.Production.Contract;
+using GeekBurger.Products.Contract;
+using GeekBurger.StoreCatalog.Model;
+using GeekBurger.StoreCatalog.Repository.Interfaces;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using GeekBurger.StoreCatalog.Repository.Interfaces;
-using GeekBurger.Production.Contract;
-using GeekBurger.Products.Contract;
 
 namespace GeekBurger.StoreCatalog.Repository
 {
     public class StoreCatalogRepository : IStoreCatalogRepository
     {
         private StoreCatalogContext _context;
-        public StoreCatalogRepository(StoreCatalogContext context)
+        private IConfiguration _configuration;
+
+        public StoreCatalogRepository(StoreCatalogContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         public IEnumerable<Product> GetProductsByRestrictions(string[] restrictions)
         {
-            throw new NotImplementedException();
+            return _context.Products.Where(p => !p.Ingredients.Any(c => restrictions.Contains(c.Name)));
         }
 
         public void UpsertProduct(ProductToGet product)
         {
-            throw new NotImplementedException();
+            var p = _context.Products.Find(product.ProductId);
+            if (p == null)
+            {
+                p = new Product();
+                _context.Products.Add(p);
+
+                p.ProductId = new Guid();
+                p.StoreId = _configuration.GetSection("Store:Id").Get<Guid>();
+            }
+
+            p.Image = product.Image;
+            p.Name = product.Name;
+            p.Price = product.Price;
+
+            p.Ingredients.Clear();
+            foreach (var item in product.Items)
+                p.Ingredients.Add(new Item { ItemId = item.ItemId, Name = item.Name });
         }
 
         public void UpsertProduction(ProductionToGet production)
         {
-            throw new NotImplementedException();
+            var p = _context.Areas.Find(production.ProductionId);
+            if (p == null)
+            {
+                p = new Area();
+                _context.Areas.Add(p);
+
+                p.AreaId = new Guid();
+            }
+
+            p.On = production.On == true;
+
+            p.Restrictions.Clear();
+            foreach (var item in production.Restrictions)
+                p.Restrictions.Add(new Restrictions { Name = item });
         }
 
         public IQueryable<Area> GetAreas()
